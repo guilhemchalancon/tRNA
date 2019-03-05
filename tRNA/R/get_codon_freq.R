86f7aefadff24387c09f2d437ed81f5487333a7e
Guilhem Chalancon <guilhem.chalancon@gmail.com>
2019-03-05 18:34:32 +0000

    get_codon_freq (WIP)

---
+++ tRNA/R/get_codon_freq.R
@@ -0,0 +1,10 @@
+source("http://www.bioconductor.org/biocLite.R")
+biocLite("seqinr")
+
+require(Biostrings)
+require(seqinr)
+
+#' Converts fasta sequence into codon frequency table
+get_codon_freq <- function(seq){
+  
+}
