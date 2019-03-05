a4234740d0b0e9dd639c8dfa47af1f626a03a009
Guilhem Chalancon <guilhem.chalancon@gmail.com>
2019-03-05 18:34:18 +0000

    get_tAI (main routine, WIP)

--- tRNA/R/get_tAI.R
+++ tRNA/R/get_tAI.R
@@ -1,4 +1,18 @@
-# 
-get_tAI <- function(){
-  
+#'
+#' @param seq a FASTA sequence
+#' @param weights 
+#' @param score
+#' @return a data.table with stAI scores 
+get_tAI <- function(seq, weights, score){
+  codons  <- colnames(x)
+  w <- weights[,score]
+  names(w) <- weights[,"codon"]
+  w <- w[codons]
+  w = log(w)      #calculate log of w
+  n = apply(x,1,'*',w)		#multiply each row of by the weights
+  n = t(n)                      #transpose
+  n = apply(n,1,sum)		#sum rows
+  L = apply(x,1,sum)		#get each ORF length
+  tAI = exp(n/L)		#get tai
+  return(tAI)
 }
